b8a60d236a18ca8ee71ad05b867887b50feacb5b
facusandd <facundosand@protonmail.com>
2023-11-06 20:36:50 -0300

    commit donde agregue la base de datos

--- app/Views/front/catalogo.php
+++ app/Views/front/catalogo.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="description" content="La magia de conducir">
@@ -11,3 +11,46 @@
   </head>
 
   <body class="bg-warning-subtle">
+  <div class="container">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="assets\img\e9db64d8070b4502a4283ad2435938b4.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Pauny 580</a>
+          </h4>
+          <p class="card-text">Nuestro mejor modelo</p>
+        </div>
+      </div>
+    </div>
+  
+
+  
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="assets/img/evo180c.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Pauny Evo 180 C</a>
+          </h4>
+          <p class="card-text">El modelo de siempre</p>
+        </div>
+      </div>
+    </div>
+  
+
+  
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="assets\img\1478705460_evo-500c.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Pauny Evo 500 C</a>
+          </h4>
+          <p class="card-text">Para los que prefieren calidad y ergonomía</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ basededatosfacu.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-11-2023 a las 00:32:56
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sand_facundo`
+--
+CREATE DATABASE IF NOT EXISTS `sand_facundo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sand_facundo`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfiles`
+--
+-- Creación: 24-10-2023 a las 16:53:55
+--
+
+CREATE TABLE `perfiles` (
+  `id_perfiles` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `perfiles`
+--
+
+INSERT INTO `perfiles` (`id_perfiles`, `descripcion`) VALUES
+(1, 'admin'),
+(2, 'cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Creación: 03-11-2023 a las 22:25:01
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `perfil_id` int(11) NOT NULL DEFAULT 2,
+  `baja` varchar(2) NOT NULL DEFAULT 'NO'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `email`, `pass`, `perfil_id`, `baja`) VALUES
+(12, 'facundo', 'facundo@gmail.com', '$2y$10$fgsKrpFXjH3GSJqFSeGxL.C6k5a47V8e2tOlfJ8k.iybTWRiBRE2K', 1, 'NO'),
+(13, 'sand', 'sand@gmail.com', '$2y$10$aRJ0lxiSsgzvDnYlpNsqfeLXufQkjmzvZkH/6g2Va3CM1/0huCPAe', 2, 'NO');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  ADD PRIMARY KEY (`id_perfiles`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `perfiles`
+--
+ALTER TABLE `perfiles`
+  MODIFY `id_perfiles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
